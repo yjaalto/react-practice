6e4afeb68d4481b7bcec668f9cd950d88e3b45da
최연정 <yjchoi@idino.co.kr>
2020-02-26 17:43:14 +0900

    - CRA 사용하기 - prop, setState 사용하기 - input 값 변경시 setState

---
+++ react-tutorial/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PhoneForm from "./PhoneForm";
+
+function App() {
+  return (
+    <div className="App">
+      <PhoneForm></PhoneForm>
+    </div>
+  );
+}
+
+export default App;
