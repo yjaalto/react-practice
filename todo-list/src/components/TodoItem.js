6add0aaf464a27bfab086118ae26bd3f8bb7231c
yjaalto <makalu171@gmail.com>
2020-03-15 16:11:56 +0900

    Todo list

---
+++ todo-list/src/components/TodoItem.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import "./TodoItem.css";
+
+class TodoItem extends Component {
+    shouldComponentUpdate(nextProps, nextState) {
+        return this.props.checked !== nextProps.checked;
+    }
+
+    render() {
+        const { text, checked, id, onToggle, onRemove } = this.props;
+
+        console.log(id);
+
+        return (
+            <div className="todo-item" onClick={() => onToggle(id)}>
+                <div
+                    className="remove"
+                    onClick={e => {
+                        e.stopPropagation();
+                        onRemove(id);
+                    }}
+                >
+                    &times;
+                </div>
+                <div className={`todo-text ${checked ? " checked" : ""}`}>
+                    <div>{text}</div>
+                </div>
+                {checked && <div className="check-mark">&#x2713;</div>}
+            </div>
+        );
+    }
+}
+const createWarning = funcName => {
+    return console.warn(funcName + " is not defined");
+};
+
+TodoItem.defaultProps = {
+    onToggle: () => createWarning("onToggle"),
+    onRemove: () => createWarning("onRemove")
+};
+export default TodoItem;
